9dd888dcfa8024e5826ee441288e82b5b9da87ac
Ashraf Essam <>
2022-10-19 22:47:33 +0200

    Initial Commit

---
+++ valU-iOS-Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  valU-iOS-Task
+//
+//  Created by Ashraf Essam on 19/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
